bed8fe4bfed460c48a1f542b318f484293721b61
Reto Schüttel <reto@schuettel.ch>
2009-04-20 21:20:00 +0200

    basic functionality finished

--- spamcademy.py
+++ spamcademy.py
@@ -1,10 +1,14 @@
-#!/usr/bin/python2.5
+#!/usr/bin/python2.6
 
 import sys
 import os
 import pwd
+import time
+import subprocess
 import logging as log
 from mailbox import Maildir
+from email.Generator import Generator
+from tempfile import NamedTemporaryFile, TemporaryFile
 
 log.basicConfig(level=log.DEBUG)
 
@@ -27,6 +31,7 @@ def change_to_user(user):
   os.environ['HOME'] = path
 
 
+LOG_DIR = "/var/tmp/log-sa"
 HOME = "test"
 FLAG = ".spamcademy"
 
@@ -54,9 +59,10 @@ for user in os.listdir(HOME):
     log.warn("%s doesn't have a maildir" % user)
     sys.exit(0)
 
-  maildir = Maildir(maildir_path)
+  maildir = Maildir(maildir_path, factory=None)
 
   if "SA" not in maildir.list_folders():
+    log.info("Creating structure for user %s" % user)
     # create initial structure
     sa = maildir.add_folder("SA")
     spam = sa.add_folder("Spam")
@@ -73,9 +79,59 @@ for user in os.listdir(HOME):
   ham_open     = sa.get_folder('Ham').get_folder('offen')
   ham_learned  = sa.get_folder('Ham').get_folder('gelernt')
 
-  #for id, msg in spam_open.items():
-  #  print id, msg
 
+  jobs = (
+    ('spam', ('spamassassin', '-r', '-D' ), spam_open, spam_learend),
+    ('ham',  ('sa-learn', '--ham', '-D' ), ham_open, ham_learned)
+  )
+
+
+  for desc, cmd, mbox_todo, mbox_done in jobs:
+    log.info  ("User %s: processing %s" % (user, desc))
+
+    for key, msg in mbox_todo.items():
+      # store mail in a temporary file
+      mail = NamedTemporaryFile()
+      g = Generator(mail, mangle_from_=False, maxheaderlen=0)
+      g.flatten(msg)
+
+      mail.flush()
+      mail.seek(0)
+
+      # move the mail to the learned folder
+      mbox_done.add(msg)
+      mbox_todo.discard(key)
+
+      std_out = NamedTemporaryFile()
+      std_err = NamedTemporaryFile()
+
+
+      start = time.time()
+      p = subprocess.Popen(cmd, stdin=mail, stdout=std_out, stderr=std_out, cwd=path)
+
+      ret = p.wait()
+      duration = time.time() - start
+
+      log.debug("User %s: cmd: %s ret: %i duration: %i msg: %s" % (user, str(cmd), ret, duration, key))
+
+      std_out.flush()
+      std_err.flush()
+
+      mail_log   = file(LOG_DIR + "/" + key + "_mail.log", "w")
+      stdout_log = file(LOG_DIR + "/" + key + "_stdout.log", "w")
+      stderr_log = file(LOG_DIR + "/" + key + "_stderr.log", "w")
+
+      std_out.seek(0)
+      std_err.seek(0)
+      mail.seek(0)
+      mail_log.write(mail.read())
+      stdout_log.write(std_out.read())
+      stderr_log.write(std_err.read())
+
+      for fh in std_err, std_out, mail, mail_log, stdout_log, stderr_log:
+        fh.close()
+
+      sys.exit(0)
   # end children here
   sys.exit(0)
 
